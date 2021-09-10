861a2930d6b7e9e51b289104fcc07f2abf0cef22
Julin ddd 011 <wellingtonjr53@outlook.com.br>
2021-09-10 16:59:53 -0300

    create initial setup and coupling all modules in this file

--- public/app.js
+++ public/app.js
@@ -1,1 +1,18 @@
-console.debug('hello world!')
+import AppController from "./src/appController.js";
+import ConnectionManager from "./src/connectionManager.js";
+import View from "./src/viewManager.js";
+
+const API_URL = "https://127.0.0.1:3000";
+
+const appController = new AppController({
+  connectionManager: new ConnectionManager({
+    apiUrl: API_URL,
+  }),
+  viewManager: new View(),
+});
+
+try {
+  await appController.initialize();
+} catch (error) {
+  console.error("error on initializing", error);
+}
